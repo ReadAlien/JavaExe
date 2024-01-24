f06dd1cb9a88fd6025bbead44373165284c91508
JaeWon <junsj1230@naver.com>
2024-01-24 15:43:15 +0900

    DataType

---
+++ Ch02_Variable/src/ch02/variable01/DataType.java
@@ -0,0 +1,20 @@
+package ch02.variable01;
+
+public class DataType {
+	public static void main(String args[]) {
+		int num = 100;
+		double dnum = 3.14;
+		char c = 'c';
+		String name = "홍길동";
+		boolean isRun = true;
+		String str = "a";
+		
+		System.out.println(num);
+		System.out.println(dnum);
+		System.out.println(c);
+		System.out.println(name);
+		System.out.println(isRun);
+		System.out.println(str);
+	
+	}
+}
